ce06bb8ea65e4770ade5580410929ac8a7bf5ab6
MiraoDaSilva <mirarodrigo@hotmail.com>
2017-03-26 19:52:21 +0100

    Added HotelCancelBookingMethodTest

---
+++ hotel/src/test/java/pt/ulisboa/tecnico/softeng/hotel/domain/HotelCancelBookingMethodTest.java
@@ -0,0 +1,81 @@
+package pt.ulisboa.tecnico.softeng.hotel.domain;
+
+import org.joda.time.LocalDate;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import pt.ulisboa.tecnico.softeng.hotel.domain.Room.Type;
+import pt.ulisboa.tecnico.softeng.hotel.exception.HotelException;
+
+public class HotelCancelBookingMethodTest {
+	
+	private final LocalDate arrival = new LocalDate(2016, 12, 19);
+	private final LocalDate departure = new LocalDate(2016, 12, 21);
+	private Hotel hotel;
+	private Room room;
+	private String reference;
+	
+	@Before
+	public void setUp() {
+		this.hotel = new Hotel("XPTO123", "Paris");
+		this.room = new Room(this.hotel, "01", Type.DOUBLE);
+		this.reference=Hotel.reserveRoom(Type.DOUBLE, arrival, departure);
+	}
+	
+	@Test(expected=HotelException.class)
+	public void nullConfirmationReference(){
+		Hotel.cancelBooking(null);
+	}
+	
+	@Test(expected=HotelException.class)
+	public void emptyConfirmationReference(){
+		Hotel.cancelBooking("");
+	}
+	
+	@Test(expected=HotelException.class)
+	public void spacesConfirmationReference(){
+		Hotel.cancelBooking("    ");
+	}
+	
+	@Test
+	public void nonExistentConfirmationReference(){
+		try{
+			Hotel.cancelBooking("doesntexist");
+			Assert.fail();
+			}
+		catch(HotelException e){
+			Assert.assertFalse(this.room.isFree(Type.DOUBLE,arrival,departure));
+		}
+	}
+	
+	@Test
+	public void duplicateCancelation(){
+		try{
+			Hotel.cancelBooking("XPTO1231");
+			Hotel.cancelBooking("XPTO1231");
+			Assert.fail();
+			}
+		catch(HotelException e){
+			Assert.assertTrue(this.room.isFree(Type.DOUBLE,arrival,departure));
+		}
+	}
+	
+	
+	
+	@Test
+	public void SuccessfulCancelation(){
+		Hotel.cancelBooking(reference);
+		Assert.assertTrue(this.room.isFree(Type.DOUBLE,arrival,departure));
+	}
+	
+	
+	
+	
+	@After
+	public void tearDown() {
+		Hotel.hotels.clear();
+	}
+}
+
