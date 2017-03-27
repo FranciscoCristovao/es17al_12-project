8f99c384c1b6c6101bbaeaf93da06ae799da46c3
JoaoSilvestre95 <joao.p.silvestre@tecnico.ulisboa.pt>
2017-03-27 02:46:42 +0100

    refatorizacao da subclasse UndoState de AdventureState

---
+++ broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/domain/UndoState.java
@@ -0,0 +1,58 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import pt.ulisboa.tecnico.softeng.activity.dataobjects.ActivityReservationData;
+import pt.ulisboa.tecnico.softeng.broker.domain.Adventure.State;
+import pt.ulisboa.tecnico.softeng.broker.exception.RemoteAccessException;
+import pt.ulisboa.tecnico.softeng.broker.interfaces.ActivityInterface;
+import pt.ulisboa.tecnico.softeng.broker.interfaces.BankInterface;
+import pt.ulisboa.tecnico.softeng.broker.interfaces.HotelInterface;
+import pt.ulisboa.tecnico.softeng.hotel.exception.HotelException;
+
+public class UndoState extends AdventureState {
+	private static Logger logger = LoggerFactory.getLogger(UndoState.class);
+
+	@Override
+	public State getState() {
+		return State.UNDO;
+	}
+	
+	@Override
+	public void process(Adventure adventure) {
+		logger.debug("process");
+		
+		if (adventure.cancelPayment()) {
+			String paymentCancellation;
+			try {
+				paymentCancellation = BankInterface.cancelPayment(adventure.getPaymentConfirmation());
+			} catch (HotelException | RemoteAccessException ex) {
+				// does not change state
+				return;
+			}
+		}
+
+		if (adventure.cancelActivity()) {
+			String activityCancellation;
+			try {
+				activityCancellation = ActivityInterface.cancelReservation(adventure.getActivityConfirmation());
+			} catch (HotelException | RemoteAccessException ex) {
+				// does not change state
+				return;
+			}
+		}
+
+		if (adventure.cancelRoom()) {
+			String roomCancellation;
+			try {
+				roomCancellation = HotelInterface.cancelBooking(adventure.getRoomConfirmation());
+			} catch (HotelException | RemoteAccessException ex) {
+				// does not change state
+				return;
+			}
+		}
+
+		if (!adventure.cancelPayment() && !adventure.cancelActivity() && !adventure.cancelRoom()) {
+			adventure.setState(State.CANCELLED);
+		}
+	}
+}
