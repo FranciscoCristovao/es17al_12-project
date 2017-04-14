b060e204793778e3bdfc42cc1599a2ce2e3f9709
SofiaAparicio <sofia.aparicio@tecnico.ulisboa.pt>
2017-04-14 16:16:37 +0100

    Made class ActivityOffer persistent closes #68

--- activity/src/main/java/pt/ulisboa/tecnico/softeng/activity/domain/ActivityOffer.java
+++ activity/src/main/java/pt/ulisboa/tecnico/softeng/activity/domain/ActivityOffer.java
@@ -5,9 +5,10 @@ import java.util.Set;
 
 import org.joda.time.LocalDate;
 
+
 import pt.ulisboa.tecnico.softeng.activity.exception.ActivityException;
 
-public class ActivityOffer {
+public class ActivityOffer extends ActivityOffer_Base{
 	private final LocalDate begin;
 	private final LocalDate end;
 	private final int capacity;
@@ -21,6 +22,11 @@ public class ActivityOffer {
 		this.capacity = activity.getCapacity();
 
 		activity.addOffer(this);
+	}
+	
+	public void delete() {
+		/*TODO: set root a null*/
+		deleteDomainObject();
 	}
 
 	private void checkArguments(Activity activity, LocalDate begin, LocalDate end) {
