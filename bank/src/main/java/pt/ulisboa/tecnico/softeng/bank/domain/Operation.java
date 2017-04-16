a61c700c9f99f0d0bf646e5cb2062982a89cd620
JoaoSilvestre95 <joao.p.silvestre@tecnico.ulisboa.pt>
2017-04-16 20:02:08 +0100

    Turn Operation class atributes persistent closes #91

--- bank/src/main/java/pt/ulisboa/tecnico/softeng/bank/domain/Operation.java
+++ bank/src/main/java/pt/ulisboa/tecnico/softeng/bank/domain/Operation.java
@@ -8,24 +8,18 @@ public class Operation extends Operation_Base{
 	public static enum Type {
 		DEPOSIT, WITHDRAW
 	};
-
 	private static int counter = 0;
-
-	private final String reference;
-	private final Type type;
+	private final Type type;	
 	private final Account account;
-	private final int value;
-	private final DateTime time;
+	
 
 	public Operation(Type type, Account account, int value) {
 		checkArguments(type, account, value);
-
-		this.reference = account.getBank().getCode() + Integer.toString(++Operation.counter);
+		this.setReference(account.getBank().getCode() + Integer.toString(++Operation.counter));
 		this.type = type;
 		this.account = account;
-		this.value = value;
-		this.time = DateTime.now();
-
+		this.setValue(value);
+		this.setTime(DateTime.now());
 		account.getBank().addLog(this);
 	}
 
@@ -35,10 +29,6 @@ public class Operation extends Operation_Base{
 		}
 	}
 
-	public String getReference() {
-		return this.reference;
-	}
-
 	public Type getType() {
 		return this.type;
 	}
@@ -46,21 +36,13 @@ public class Operation extends Operation_Base{
 	public Account getAccount() {
 		return this.account;
 	}
-
-	public int getValue() {
-		return this.value;
-	}
-
-	public DateTime getTime() {
-		return this.time;
-	}
-
+	
 	public String revert() {
 		switch (this.type) {
 		case DEPOSIT:
-			return this.account.withdraw(this.value);
+			return this.account.withdraw(this.getValue());
 		case WITHDRAW:
-			return this.account.deposit(this.value);
+			return this.account.deposit(this.getValue());
 		default:
 			throw new BankException();
 
