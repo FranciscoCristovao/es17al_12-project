cab5da7503f3faee19c37212e44faf5e43a64ffb
PedroCaldeira <pedromiguelcc@tecnico.ulisboa.pt>
2017-04-21 20:48:56 +0100

    Test Client Persistence on BankPersistence Test fix #94

--- bank/src/test/java/pt/ulisboa/tecnico/softeng/bank/domain/BankPersistenceTest.java
+++ bank/src/test/java/pt/ulisboa/tecnico/softeng/bank/domain/BankPersistenceTest.java
@@ -3,6 +3,7 @@ package pt.ulisboa.tecnico.softeng.bank.domain;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Test;
 
 import pt.ist.fenixframework.Atomic;
@@ -10,8 +11,10 @@ import pt.ist.fenixframework.Atomic.TxMode;
 import pt.ist.fenixframework.FenixFramework;
 
 public class BankPersistenceTest {
+	private static final String BANK_NAME = "Money";
 	private static final String BANK_CODE = "BK01";
-
+	private static final String CLIENT_NAME = "Kelson";
+	/*private String clientID*/
 	@Test
 	public void success() {
 		atomicProcess();
@@ -20,14 +23,28 @@ public class BankPersistenceTest {
 
 	@Atomic(mode = TxMode.WRITE)
 	public void atomicProcess() {
-		new Bank("Money", BANK_CODE);
+		Bank bank =new Bank(BANK_NAME, BANK_CODE);
+		/*clientID = */new Client(bank,CLIENT_NAME)/*.getID()*/;
 	}
 
 	@Atomic(mode = TxMode.READ)
 	public void atomicAssert() {
 		Bank bank = Bank.getBankByCode(BANK_CODE);
-
-		assertEquals("Money", bank.getName());
+		 
+		assertEquals(BANK_NAME, bank.getName());
+		
+		
+		Client client = null;
+		for (Client c : bank.getClientSet()){
+			client = c;
+		}
+		
+		Assert.assertFalse(client==null);
+		assertEquals(bank,client.getBank());
+		/*assertEquals(clientID,client.getID())*/
+		Assert.assertTrue(client.getID().length()>=1);
+		assertEquals(CLIENT_NAME,client.getName());
+		
 	}
 
 	@After
