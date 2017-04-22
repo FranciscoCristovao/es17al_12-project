21142f7210b17604df0443388c575c8a24201c1d
Jpmcrespo <jpmcrespo@gmail.com>
2017-04-22 23:09:37 +0100

    Add Room tests to HotelPersistenceTest closes #50

--- hotel/src/test/java/pt/ulisboa/tecnico/softeng/hotel/domain/HotelPersistenceTest.java
+++ hotel/src/test/java/pt/ulisboa/tecnico/softeng/hotel/domain/HotelPersistenceTest.java
@@ -3,6 +3,9 @@ package pt.ulisboa.tecnico.softeng.hotel.domain;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Test;
 
@@ -12,6 +15,7 @@ import pt.ist.fenixframework.FenixFramework;
 
 public class HotelPersistenceTest {
 	private static final String HOTEL_CODE = "BK01234";
+	private static final String ROOM_NUMBER = "007";
 
 	@Test
 	public void success() {
@@ -21,7 +25,9 @@ public class HotelPersistenceTest {
 
 	@Atomic(mode = TxMode.WRITE)
 	public void atomicProcess() {
-		new Hotel(HOTEL_CODE, "Money");
+		Hotel hotel= new Hotel(HOTEL_CODE, "Money");
+		
+		new Room(hotel, "007", Room.Type.SINGLE);
 	}
 
 	@Atomic(mode = TxMode.READ)
@@ -30,6 +36,15 @@ public class HotelPersistenceTest {
 		
 		assertNotNull(hotel);
 		assertEquals("Money", hotel.getName());
+		
+		assertEquals(1, hotel.getRoomSet().size());
+		List<Room> rooms= new ArrayList<>(hotel.getRoomSet());
+		Room room = rooms.get(0);
+		
+		assertNotNull(room);
+		assertEquals(ROOM_NUMBER, room.getNumber());
+		assertEquals(hotel, room.getHotel());
+		assertEquals(Room.Type.SINGLE, room.getType());
 	}
 
 	@After
