3f433169f74aa59770b98010949c8a875a824720
PedroCaldeira <pedromiguelcc@tecnico.ulisboa.pt>
2017-04-30 17:06:07 +0100

    Implement Provider Creation closes #1

--- activity/src/main/java/pt/ulisboa/tecnico/softeng/activity/presentation/ProviderController.java
+++ activity/src/main/java/pt/ulisboa/tecnico/softeng/activity/presentation/ProviderController.java
@@ -4,10 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import pt.ulisboa.tecnico.softeng.activity.exception.ActivityException;
 import pt.ulisboa.tecnico.softeng.activity.services.local.ActivityInterface;
+import pt.ulisboa.tecnico.softeng.activity.services.local.dataobjects.ProviderData;
 
 
 
@@ -19,8 +22,23 @@ public class ProviderController {
 	@RequestMapping(method = RequestMethod.GET)
 	public String providerForm(Model model) {
 		logger.info("providerForm");
-		model.addAttribute("provider");
+		model.addAttribute("provider",new ProviderData());
 		model.addAttribute("providers", ActivityInterface.getProviders());
 		return "providers";
+	}
+	@RequestMapping(method = RequestMethod.POST)
+	public String providerSubmit(Model model, @ModelAttribute ProviderData providerData) {
+		logger.info("providerSubmit name:{}, code:{}", providerData.getName(), providerData.getCode());
+
+		try {
+			ActivityInterface.createProvider(providerData);
+		} catch (ActivityException ae) {
+			model.addAttribute("error", "Error: it was not possible to create the provider");
+			model.addAttribute("provider", providerData);
+			model.addAttribute("providers", ActivityInterface.getProviders());
+			return "providers";
+		}
+
+		return "redirect:/providers";
 	}
 }
