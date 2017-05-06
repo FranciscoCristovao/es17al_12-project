0a7fa587cd56b0b29d61b0558ac0703234e934c6
FranciscoCristovao <francisco.m.cristovao@gmail.com>
2017-05-06 17:24:27 +0100

    Create hotel closes #120

--- hotel/src/main/java/pt/ulisboa/tecnico/softeng/hotel/presentation/HotelController.java
+++ hotel/src/main/java/pt/ulisboa/tecnico/softeng/hotel/presentation/HotelController.java
@@ -4,9 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import pt.ulisboa.tecnico.softeng.hotel.exception.HotelException;
 import pt.ulisboa.tecnico.softeng.hotel.services.local.HotelInterface;
 import pt.ulisboa.tecnico.softeng.hotel.services.local.dataobjects.HotelData;
 
@@ -21,6 +23,23 @@ public class HotelController {
 		model.addAttribute("hotel", new HotelData());
 		model.addAttribute("hotels", HotelInterface.getHotels());
 		return "hotels";
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public String hotelSubmit(Model model, @ModelAttribute HotelData hotel) {
+		logger.info("hotelSubmit code:{}, name:{}", hotel.getCode(), hotel.getName());
+
+		try {
+			HotelInterface.createHotel(hotel);
+		} catch (HotelException he) {
+			he.printStackTrace();
+			model.addAttribute("error", "Error: it was not possible to create the hotel");
+			model.addAttribute("hotel", hotel);
+			model.addAttribute("hotels", HotelInterface.getHotels());
+			return "hotels";
+		}
+
+		return "redirect:/hotels";
 	}
 
 }
